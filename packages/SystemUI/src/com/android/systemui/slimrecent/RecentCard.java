SlimRoms/frameworks_base

Frameworks: Slim Recents app screen (1/2)
One of the parts on AOSP google missed to update/improve in the last
couple of releases is the recent app screen to allow better and faster
multitasking. I had always a special vision how it could/should look
and gives the user a faster workflow.

This commit is now our new recent screen. It is a complete new implementation
and replaces googles AOSP recent app screen. The code changes in the base itself
are quiet miniml to call the new recent screen.

Some notes:

1. This patch needs a new repo: frameworks_opt_cards which is a central repo
   to get any kind of googles cards style. Thanks Gabrielle Mariotti a ton who
   made the base for this library and hellped a lot out.  We just fixed together with him some parts
   and mad it useable as AOSP central library. So we can use it later for other
   changes as well

2. Preload needs to work 100%. So it is needed to check that the calling parts
   (like hw key rebinding, pie, navigation bar) do a proper preload. If they do not
   recent screen will just not show up. To follow what they are doing you can enable
   debug in RecentController.java which exactly shows what was called.

Some notes to the patch itself:

- Recent app  screen slides from the right edge into the screen

- Show recent apps in new google card style. Most recent apps show
  expanded with the app screenshot. Other are collapsed. Of course
  the user can change expanded/collapse of each app during the session where the state
  will be restored on next access.

- Compared to AOSP recents we do a 3 stage loading
  1. step: preload the task list and build or update the cards (does not load any content at this point)
  2. step: User actually toggled the recent screen....icons, text and screenshot thumbnail which will get visible are loaded now
  3. step: Recent screen is fully animated in. All other non visible content at this time are loaded now.
  This allows a smooth experience for the user and a minimal load 

packages/SystemUI/src/com/android/systemui/slimrecent/RecentCard.java
@@ -1,0 +1,159 @@
+/*
+ * Copyright (C) 2014-2015 SlimRoms Project
+ * Author: Lars Greiss - email: kufikugel@googlemail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.android.systemui.slimrecent;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.cards.internal.Card;
+
+import com.android.systemui.R;
+
+/**
+ * This class handles our base card view.
+ */
+public class RecentCard extends Card {
+
+    private RecentHeader mHeader;
+    private RecentAppIcon mRecentIcon;
+    private RecentExpandedCard mExpandedCard;
+
+    private int mPersistentTaskId;
+
+    private int defaultCardBg = mContext.getResources().getColor(
+                R.color.recents_task_bar_default_background_color);
+    private int cardColor = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.RECENT_CARD_BG_COLOR,
+                defaultCardBg, UserHandle.USER_CURRENT);
+
+    public RecentCard(Context context, TaskDescription td, float scaleFactor) {
+        this(context, R.layout.inner_base_main, td, scaleFactor);
+    }
+
+    public RecentCard(Context context, int innerLayout, TaskDescription td, float scaleFactor) {
+        super(context, innerLayout);
+
+        constructBaseCard(context, td, scaleFactor);
+    }
+
+    // Construct our card.
+    private void constructBaseCard(Context context,
+            final TaskDescription td, float scaleFactor) {
+
+        // Construct card header view.
+        mHeader = new RecentHeader(mContext, td, scaleFactor);
+
+        // Construct app icon view.
+        mRecentIcon = new RecentAppIcon(
+                context, td.resolveInfo, td.identifier, scaleFactor, td.getIsFavorite());
+        mRecentIcon.setExternalUsage(true);
+
+        // Construct expanded area view.
+        mExpandedCard = new RecentExpandedCard(context, td, scaleFactor);
+        initExpandedState(td);
+
+        // set custom background
+        if (cardColor != 0x00ffffff) {
+            this.setBackgroundResource(new ColorDrawable(cardColor));
+        } else {
+            this.setBackgroundResource(new ColorDrawable(getDefaultCardColorBg(td)));
+        }
+
+        // Finally add header, icon and expanded area to our card.
+        addCardHeader(mHeader);
+        addCardThumbnail(mRecentIcon);
+        addCardExpand(mExpandedCard);
+
+        mPersistentTaskId = td.persistentTaskId;
+    }
+
+    /** Returns the activity's primary color. */
+    public int getDefaultCardColorBg(TaskDescription td) {
+        if (td != null && td.cardColor != 0) {
+            return td.cardColor;
+        }
+        return defaultCardBg;
+    }
+
+    // Update content of our card.
+    public void updateCardContent(final TaskDescription td, float scaleFactor) {
+        if (mHeader != null) {
+            // Set or update the header title.
+            mHeader.updateHeader(td, scaleFactor);
+        }
+        if (mRecentIcon != null) {
+            mRecentIcon.updateIcon(
+                    td.resolveInfo, td.identifier, scaleFactor, td.getIsFavorite());
+        }
+        if (mExpandedCard != null) {
+            // Set expanded state.
+            initExpandedState(td);
+            // Update app screenshot.
+            mExpandedCard.updateExpandedContent(td, scaleFactor);
+        }
+        mPersistentTaskId = td.persistentTaskId;
+
+        // set custom background
+        if (cardColor != 0x00ffffff) {
+            this.setBackgroundResource(new ColorDrawable(cardColor));
+        } else {
+            this.setBackgroundResource(new ColorDrawable(getDefaultCardColorBg(td)));
+        }
+    }
+
+    // Set initial expanded state of our card.
+    private void initExpandedState(TaskDescription td) {
+        // Read flags and set accordingly initial expanded state.
+        final boolean isTopTask =
+                (td.getExpandedState() & RecentPanelView.EXPANDED_STATE_TOPTASK) != 0;
+
+        final boolean isSystemExpanded =
+                (td.getExpandedState() & RecentPanelView.EXPANDED_STATE_BY_SYSTEM) != 0;
+
+        final boolean isUserExpanded =
+                (td.getExpandedState() & RecentPanelView.EXPANDED_STATE_EXPANDED) != 0;
+
+        final boolean isUserCollapsed =
+                (td.getExpandedState() & RecentPanelView.EXPANDED_STATE_COLLAPSED) != 0;
+
+        final boolean isExpanded =
+                ((isSystemExpanded && !isUserCollapsed) || isUserExpanded) && !isTopTask;
+
+        if (mHeader != null) {
+            // Set visible the expand/collapse button.
+            mHeader.setButtonExpandVisible(!isTopTask);
+        }
+
+        setExpanded(isExpanded);
+    }
+
+    @Override
+    public void setupInnerViewElements(ViewGroup parent, View view) {
+        // Nothing to do here.
+        return;
+    }
+
+    public int getPersistentTaskId() {
+        return mPersistentTaskId;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/slimrecent/RecentHeader.java
@@ -1,0 +1,158 @@
+/*
+ * Copyright (C) 2014-2015 SlimRoms Project
+ * Author: Lars Greiss - email: kufikugel@googlemail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.android.systemui.slimrecent;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.cards.internal.CardHeader;
+import com.android.systemui.R;
+import com.android.systemui.recents.misc.Utilities;
+
+
+/**
+ * This class handles the header view.
+ */
+public class RecentHeader extends CardHeader {
+
+    private Context mContext;
+
+    private TaskDescription mTaskDescription;
+
+    private String mLabel;
+    private int mHeaderHeight;
+    private int mHeaderWidth;
+    private float mHeaderTextSize;
+    private int mExpandedButtonWidth;
+    private float mScaleFactor;
+    private boolean mScaleFactorChanged;
+
+    public RecentHeader(Context context, TaskDescription td, float scaleFactor) {
+        super(context);
+        mTaskDescription = td;
+        mContext = context;
+        mLabel = td.getLabel();
+        mScaleFactor = scaleFactor;
+        initDimensions();
+
+        Drawable d = mContext.getResources().getDrawable(R.drawable.card_menu_button_expand);
+        mExpandedButtonWidth = d.getIntrinsicWidth();
+    }
+
+    // Update header content.
+    public void updateHeader(TaskDescription td, float scaleFactor) {
+        mTaskDescription = td;
+        mLabel = td.getLabel();
+        if (scaleFactor != mScaleFactor) {
+            mScaleFactorChanged = true;
+            mScaleFactor = scaleFactor;
+            initDimensions();
+        }
+
+    }
+
+    // Set initial dimensions.
+    private void initDimensions() {
+        Resources res = mContext.getResources();
+        mHeaderHeight = (int) (res.getDimensionPixelSize(
+                R.dimen.recent_app_icon_size) * mScaleFactor);
+        mHeaderWidth = (int) (res.getDimensionPixelSize(
+                R.dimen.recent_header_width) * mScaleFactor);
+        mHeaderTextSize = res.getDimensionPixelSize(
+                R.dimen.recent_text_size) * mScaleFactor;
+    }
+
+    /**
+     * Assign the label to the view.
+     */
+    @Override
+    public void setupInnerViewElements(ViewGroup parent, View view) {
+        if (view == null || mLabel == null) {
+            return;
+        }
+
+        // We use here a view holder to reduce expensive findViewById calls
+        // when getView is called on the arrayadapter which calls setupInnerViewElements.
+        // Simply just check if the given view was already tagged. If yes we know it has
+        // the appIconView we want to have. If not we search it, give it to the viewholder
+        // and tag the view for the next call to reuse the holded information later.
+        ViewHolder holder;
+        holder = (ViewHolder) view.getTag();
+
+        if (holder == null) {
+            holder = new ViewHolder();
+            holder.textView = (TextView) view.findViewById(R.id.card_header_inner_simple_title);
+            // Take scale factor into account if it is different then default or it has changed.
+            if (mScaleFactor != RecentController.DEFAULT_SCALE_FACTOR || mScaleFactorChanged) {
+                mScaleFactorChanged = false;
+                int diff = 0;
+                // We have on static element in the header (expanded icon button). Take this into
+                // account to calculate the needed width.
+                if (mScaleFactor < RecentController.DEFAULT_SCALE_FACTOR) {
+                    diff = (int) ((mExpandedButtonWidth
+                            - mExpandedButtonWidth * mScaleFactor) * 2);
+                }
+                final ViewGroup.LayoutParams layoutParams = holder.textView.getLayoutParams();
+                layoutParams.width = mHeaderWidth - diff;
+                layoutParams.height = mHeaderHeight;
+                holder.textView.setLayoutParams(layoutParams);
+                holder.textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mHeaderTextSize);
+            }
+            view.setTag(holder);
+        }
+        int defaultCardText = mContext.getResources().getColor(
+                R.color.card_text_color_header);
+        int textColor = Settings.System.getIntForUser( mContext.getContentResolver(),
+                Settings.System.RECENT_CARD_TEXT_COLOR,
+                defaultCardText, UserHandle.USER_CURRENT);
+        holder.textView.setText(mLabel);
+        if (textColor != 0x00ffffff) {
+            holder.textView.setTextColor(textColor);
+        } else {
+            holder.textView.setTextColor(getDefaultTextColor());
+        }
+    }
+
+    public int getDefaultTextColor() {
+        if (mTaskDescription.cardColor != 0) {
+            if (Utilities.computeContrastBetweenColors(mTaskDescription.cardColor,
+                    Color.WHITE) < 3f) {
+                return mContext.getResources().getColor(
+                        R.color.recents_task_bar_dark_text_color);
+            } else {
+                return mContext.getResources().getColor(
+                        R.color.recents_task_bar_light_text_color);
+            }
+        }
+        return mContext.getResources().getColor(
+                    R.color.card_text_color_header);
+    }
+
+    static class ViewHolder {
+        TextView textView;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -486,6 +486,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.PIE_CONTROLS), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.USE_SLIM_RECENTS), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.RECENT_CARD_BG_COLOR), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.RECENT_CARD_TEXT_COLOR), false, this,
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -550,6 +559,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.NAVIGATION_BAR_CAN_MOVE))) {
                 prepareNavigationBarView();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.USE_SLIM_RECENTS))) {
+                updateRecents();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.RECENT_CARD_BG_COLOR))
+                    || uri.equals(Settings.System.getUriFor(
+                    Settings.System.RECENT_CARD_TEXT_COLOR))) {
+                rebuildRecentsScreen();
             }
         }
 

gmillz/frameworks_base-2

packages/SystemUI/src/com/android/systemui/slimrecent/RecentCard.java
@@ -1,0 +1,160 @@
+/*
+ * Copyright (C) 2014-2015 SlimRoms Project
+ * Author: Lars Greiss - email: kufikugel@googlemail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.android.systemui.slimrecent;
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.os.UserHandle;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.cards.internal.Card;
+
+import com.android.systemui.R;
+
+import org.slim.provider.SlimSettings;
+
+/**
+ * This class handles our base card view.
+ */
+public class RecentCard extends Card {
+
+    private RecentHeader mHeader;
+    private RecentAppIcon mRecentIcon;
+    private RecentExpandedCard mExpandedCard;
+
+    private int mPersistentTaskId;
+
+    private int defaultCardBg = mContext.getResources().getColor(
+                R.color.recents_task_bar_default_background_color);
+    private int cardColor = SlimSettings.System.getIntForUser(
+                mContext.getContentResolver(), SlimSettings.System.RECENT_CARD_BG_COLOR,
+                defaultCardBg, UserHandle.USER_CURRENT);
+
+    public RecentCard(Context context, TaskDescription td, float scaleFactor) {
+        this(context, R.layout.inner_base_main, td, scaleFactor);
+    }
+
+    public RecentCard(Context context, int innerLayout, TaskDescription td, float scaleFactor) {
+        super(context, innerLayout);
+
+        constructBaseCard(context, td, scaleFactor);
+    }
+
+    // Construct our card.
+    private void constructBaseCard(Context context,
+            final TaskDescription td, float scaleFactor) {
+
+        // Construct card header view.
+        mHeader = new RecentHeader(mContext, td, scaleFactor);
+
+        // Construct app icon view.
+        mRecentIcon = new RecentAppIcon(
+                context, td.resolveInfo, td.identifier, scaleFactor, td.getIsFavorite());
+        mRecentIcon.setExternalUsage(true);
+
+        // Construct expanded area view.
+        mExpandedCard = new RecentExpandedCard(context, td, scaleFactor);
+        initExpandedState(td);
+
+        // set custom background
+        if (cardColor != 0x00ffffff) {
+            this.setBackgroundResource(new ColorDrawable(cardColor));
+        } else {
+            this.setBackgroundResource(new ColorDrawable(getDefaultCardColorBg(td)));
+        }
+
+        // Finally add header, icon and expanded area to our card.
+        addCardHeader(mHeader);
+        addCardThumbnail(mRecentIcon);
+        addCardExpand(mExpandedCard);
+
+        mPersistentTaskId = td.persistentTaskId;
+    }
+
+    /** Returns the activity's primary color. */
+    public int getDefaultCardColorBg(TaskDescription td) {
+        if (td != null && td.cardColor != 0) {
+            return td.cardColor;
+        }
+        return defaultCardBg;
+    }
+
+    // Update content of our card.
+    public void updateCardContent(final TaskDescription td, float scaleFactor) {
+        if (mHeader != null) {
+            // Set or update the header title.
+            mHeader.updateHeader(td, scaleFactor);
+        }
+        if (mRecentIcon != null) {
+            mRecentIcon.updateIcon(
+                    td.resolveInfo, td.identifier, scaleFactor, td.getIsFavorite());
+        }
+        if (mExpandedCard != null) {
+            // Set expanded state.
+            initExpandedState(td);
+            // Update app screenshot.
+            mExpandedCard.updateExpandedContent(td, scaleFactor);
+        }
+        mPersistentTaskId = td.persistentTaskId;
+
+        // set custom background
+        if (cardColor != 0x00ffffff) {
+            this.setBackgroundResource(new ColorDrawable(cardColor));
+        } else {
+            this.setBackgroundResource(new ColorDrawable(getDefaultCardColorBg(td)));
+        }
+    }
+
+    // Set initial expanded state of our card.
+    private void initExpandedState(TaskDescription td) {
+        // Read flags and set accordingly initial expanded state.
+        final boolean isTopTask =
+                (td.getExpandedState() & RecentPanelView.EXPANDED_STATE_TOPTASK) != 0;
+
+        final boolean isSystemExpanded =
+                (td.getExpandedState() & RecentPanelView.EXPANDED_STATE_BY_SYSTEM) != 0;
+
+        final boolean isUserExpanded =
+                (td.getExpandedState() & RecentPanelView.EXPANDED_STATE_EXPANDED) != 0;
+
+        final boolean isUserCollapsed =
+                (td.getExpandedState() & RecentPanelView.EXPANDED_STATE_COLLAPSED) != 0;
+
+        final boolean isExpanded =
+                ((isSystemExpanded && !isUserCollapsed) || isUserExpanded) && !isTopTask;
+
+        if (mHeader != null) {
+            // Set visible the expand/collapse button.
+            mHeader.setButtonExpandVisible(!isTopTask);
+        }
+
+        setExpanded(isExpanded);
+    }
+
+    @Override
+    public void setupInnerViewElements(ViewGroup parent, View view) {
+        // Nothing to do here.
+        return;
+    }
+
+    public int getPersistentTaskId() {
+        return mPersistentTaskId;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/slimrecent/RecentHeader.java
@@ -1,0 +1,158 @@
+/*
+ * Copyright (C) 2014-2015 SlimRoms Project
+ * Author: Lars Greiss - email: kufikugel@googlemail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.android.systemui.slimrecent;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.UserHandle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.cards.internal.CardHeader;
+import com.android.systemui.R;
+import com.android.systemui.recents.misc.Utilities;
+
+import org.slim.provider.SlimSettings;
+
+/**
+ * This class handles the header view.
+ */
+public class RecentHeader extends CardHeader {
+
+    private Context mContext;
+
+    private TaskDescription mTaskDescription;
+
+    private String mLabel;
+    private int mHeaderHeight;
+    private int mHeaderWidth;
+    private float mHeaderTextSize;
+    private int mExpandedButtonWidth;
+    private float mScaleFactor;
+    private boolean mScaleFactorChanged;
+
+    public RecentHeader(Context context, TaskDescription td, float scaleFactor) {
+        super(context);
+        mTaskDescription = td;
+        mContext = context;
+        mLabel = td.getLabel();
+        mScaleFactor = scaleFactor;
+        initDimensions();
+
+        Drawable d = mContext.getResources().getDrawable(R.drawable.card_menu_button_expand);
+        mExpandedButtonWidth = d.getIntrinsicWidth();
+    }
+
+    // Update header content.
+    public void updateHeader(TaskDescription td, float scaleFactor) {
+        mTaskDescription = td;
+        mLabel = td.getLabel();
+        if (scaleFactor != mScaleFactor) {
+            mScaleFactorChanged = true;
+            mScaleFactor = scaleFactor;
+            initDimensions();
+        }
+
+    }
+
+    // Set initial dimensions.
+    private void initDimensions() {
+        Resources res = mContext.getResources();
+        mHeaderHeight = (int) (res.getDimensionPixelSize(
+                R.dimen.recent_app_icon_size) * mScaleFactor);
+        mHeaderWidth = (int) (res.getDimensionPixelSize(
+                R.dimen.recent_header_width) * mScaleFactor);
+        mHeaderTextSize = res.getDimensionPixelSize(
+                R.dimen.recent_text_size) * mScaleFactor;
+    }
+
+    /**
+     * Assign the label to the view.
+     */
+    @Override
+    public void setupInnerViewElements(ViewGroup parent, View view) {
+        if (view == null || mLabel == null) {
+            return;
+        }
+
+        // We use here a view holder to reduce expensive findViewById calls
+        // when getView is called on the arrayadapter which calls setupInnerViewElements.
+        // Simply just check if the given view was already tagged. If yes we know it has
+        // the appIconView we want to have. If not we search it, give it to the viewholder
+        // and tag the view for the next call to reuse the holded information later.
+        ViewHolder holder;
+        holder = (ViewHolder) view.getTag();
+
+        if (holder == null) {
+            holder = new ViewHolder();
+            holder.textView = (TextView) view.findViewById(R.id.card_header_inner_simple_title);
+            // Take scale factor into account if it is different then default or it has changed.
+            if (mScaleFactor != RecentController.DEFAULT_SCALE_FACTOR || mScaleFactorChanged) {
+                mScaleFactorChanged = false;
+                int diff = 0;
+                // We have on static element in the header (expanded icon button). Take this into
+                // account to calculate the needed width.
+                if (mScaleFactor < RecentController.DEFAULT_SCALE_FACTOR) {
+                    diff = (int) ((mExpandedButtonWidth
+                            - mExpandedButtonWidth * mScaleFactor) * 2);
+                }
+                final ViewGroup.LayoutParams layoutParams = holder.textView.getLayoutParams();
+                layoutParams.width = mHeaderWidth - diff;
+                layoutParams.height = mHeaderHeight;
+                holder.textView.setLayoutParams(layoutParams);
+                holder.textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, mHeaderTextSize);
+            }
+            view.setTag(holder);
+        }
+        int defaultCardText = mContext.getResources().getColor(
+                R.color.card_text_color_header);
+        int textColor = SlimSettings.System.getIntForUser(mContext.getContentResolver(),
+                SlimSettings.System.RECENT_CARD_TEXT_COLOR,
+                defaultCardText, UserHandle.USER_CURRENT);
+        holder.textView.setText(mLabel);
+        if (textColor != 0x00ffffff) {
+            holder.textView.setTextColor(textColor);
+        } else {
+            holder.textView.setTextColor(getDefaultTextColor());
+        }
+    }
+
+    public int getDefaultTextColor() {
+        if (mTaskDescription.cardColor != 0) {
+            if (Utilities.computeContrastBetweenColors(mTaskDescription.cardColor,
+                    Color.WHITE) < 3f) {
+                return mContext.getResources().getColor(
+                        R.color.recents_task_bar_dark_text_color);
+            } else {
+                return mContext.getResources().getColor(
+                        R.color.recents_task_bar_light_text_color);
+            }
+        }
+        return mContext.getResources().getColor(
+                    R.color.card_text_color_header);
+    }
+
+    static class ViewHolder {
+        TextView textView;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -418,6 +418,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             resolver.registerContentObserver(SlimSettings.System.getUriFor(
                     SlimSettings.System.MENU_VISIBILITY),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.USE_SLIM_RECENTS), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.RECENT_CARD_BG_COLOR), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(SlimSettings.System.getUriFor(
+                    SlimSettings.System.RECENT_CARD_TEXT_COLOR), false, this,
+                    UserHandle.USER_ALL);
             update();
         }
 
@@ -449,6 +458,14 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             } else if (uri.equals(SlimSettings.System.getUriFor(
                     SlimSettings.System.NAVIGATION_BAR_SHOW))) {
                 updateNavigationBarVisibility();
+            } else if (uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.USE_SLIM_RECENTS))) {
+                updateRecents();
+            } else if (uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.RECENT_CARD_BG_COLOR))
+                    || uri.equals(SlimSettings.System.getUriFor(
+                    SlimSettings.System.RECENT_CARD_TEXT_COLOR))) {
+                rebuildRecentsScreen();
             }
         }
 